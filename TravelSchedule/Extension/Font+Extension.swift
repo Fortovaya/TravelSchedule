e96829861bf1c8ea9b011a433a577118147d0e25
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-08-24 17:34:57 +0300

    Add new items to Font+Extension

---
+++ TravelSchedule/Extension/Font+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  Font+Extension.swift
+//  TravelSchedule
+//
+//  Created by Алина on 23.08.2025.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    static var bold17: Font {
+        Font.system(size: 17, weight: .bold)
+    }
+    
+    static var bold24: Font {
+        Font.system(size: 24, weight: .bold)
+    }
+    
+    static var bold34: Font {
+        Font.system(size: 34, weight: .bold)
+    }
+    
+    static var regular12: Font {
+        Font.system(size: 12, weight: .regular)
+    }
+    
+    static var regular17: Font {
+        Font.system(size: 17, weight: .regular)
+    }
+    
+    static var regular20: Font {
+        Font.system(size: 20, weight: .regular)
+    }
+}
