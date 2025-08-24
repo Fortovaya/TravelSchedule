4fd944f986b363ffd10b7f2265abd8c790932a22
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-08-24 17:34:28 +0300

    Add new items to CGFloat+Extension

---
+++ TravelSchedule/Extension/CGFloat+Extension..swift
@@ -0,0 +1,13 @@
+//
+//  CGFloat+Extension..swift
+//  TravelSchedule
+//
+//  Created by Алина on 23.08.2025.
+//
+
+import SwiftUI
+
+extension CGFloat {
+    static let progressBarCornerRadius: CGFloat = 3
+    static let progressBarHeight: CGFloat = 6
+}
