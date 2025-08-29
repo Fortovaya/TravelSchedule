2dafe065bd1f69badee3324783cea6d4c9f8e56d
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-08-29 20:22:48 +0300

    refactor(mvvm): make ThemePreferencesStore Sendable

--- TravelSchedule/View/MainScreen/UserDefaultsThemePreferencesStore.swift
+++ TravelSchedule/View/MainScreen/UserDefaultsThemePreferencesStore.swift
@@ -7,14 +7,18 @@
 
 import Foundation
 
-protocol ThemePreferencesStore {
+protocol ThemePreferencesStore: Sendable {
     var isDarkThemeEnabled: Bool { get set }
     var didBootstrapTheme: Bool { get set }
 }
 
-struct UserDefaultsThemePreferencesStore: ThemePreferencesStore {
+struct UserDefaultsThemePreferencesStore: ThemePreferencesStore, @unchecked Sendable {
     private let ud: UserDefaults
-    init(userDefaults: UserDefaults = .standard) { self.ud = userDefaults }
+    
+    init(userDefaults: UserDefaults = .standard) {
+        self.ud = userDefaults
+    }
+    
     
     var isDarkThemeEnabled: Bool {
         get { ud.bool(forKey: AppStorageKeys.isDarkThemeEnabled) }
